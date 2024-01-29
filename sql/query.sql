caf04fa9eccc8d092944635ae87f40fd867d5a2a
Vyacheslav Kapitonov <vyach.kapitonov@gmail.com>
2024-01-29 22:25:02 +0300

    Change naming

--- sql/query.sql
+++ sql/query.sql
@@ -1,23 +1,23 @@
 drop table if exists dinis;
 
-create table if not exists dinis as 
+create table if not exists dinis as
 with C as (
 select 30000.0 as price
 , 2000 as fixed_price
 , 15 as fixed_minutes
 ),
 calc as (
-select 
-	user_id 
-	, first_name 
-	, last_name 
+select
+	user_id
+	, first_name
+	, last_name
 	, CEIL(game_time / 60) game_time_minutes
 	, tour
 	, CEIL(C.price / COUNT(user_id) over w) cost_now
 	, ceil((C.price / (48 * 5 * 60)) * game_time) cost_per_second
-	, CEIL(CASE 
+	, CEIL(CASE
 		when game_time < C.fixed_minutes * 60 then C.fixed_price
-		else 
+		else
 		(C.price - ((COUNT(user_id) over w - sum(CASE when (game_time > C.fixed_minutes * 60) then 1 else 0 END) over w) * C.fixed_price))
 			/ (sum(CASE when (game_time > C.fixed_minutes * 60) then 1 else 0 END) over w)
 	end) as with_fixed_price
@@ -28,20 +28,19 @@ window w as (PARTITION by tour, team_id)
 )
 select
 	calc.*
-	, cost_now - calc.cost_per_second as diff_1
-	, cost_now - calc.with_fixed_price as diff_2
-	, sum(calc.cost_per_second) over w check_1
-	, sum(calc.with_fixed_price) over w check_2
+	, cost_now - calc.cost_per_second as diff_cost_per_second
+	, cost_now - calc.with_fixed_price as diff_with_fixed_price
+	, sum(calc.cost_per_second) over w check_cost_per_second
+	, sum(calc.with_fixed_price) over w check_with_fixed_price
 	, sum(cost_now) over w1 as total_now
 	, sum(cost_per_second) over w1 as total_per_second
 	, sum(with_fixed_price) over w1 as total_with_fixed_price
 from calc
 where 1=1
-window 
+window
 	w as (PARTITION by tour)
 	, w1 as (PARTITION by user_id)
 order by tour;
 
 
 select * from dinis;
-
